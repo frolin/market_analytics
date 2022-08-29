8261d11a6951f1cfd88ab11539f2e2eca3eb00b7
frolin <fralunia@gmail.com>
2022-08-29 13:15:39 +0300

    cart position stop

--- app/processes/products/wb/cart_position.rb
+++ app/processes/products/wb/cart_position.rb
@@ -1,20 +1,18 @@
-module Products
-  module WB
-    class CartPosition < ActiveInteraction::Base
-      record :product
-      string :search
-
-      WB_SEARCH = "https://www.wildberries.ru/catalog/0/search.aspx?sort=popular&search=#{search}"
-
-
-      def execute
-        page = Browser.new(WB_SEARCH).run
-        position = page.find_element(xpath: '//*[@id="c111885412"]').attribute("data-card-index")
-
-
-        product.keywords.find_by(name: search)
-      end
-
-    end
-  end
-end
+# module Products
+#   module WB
+#     class CartPosition < ActiveInteraction::Base
+#       record :product
+#       string :search
+#
+#       WB_SEARCH = "https://www.wildberries.ru/catalog/0/search.aspx?sort=popular&search=#{search}"
+#
+#       def execute
+#         page = Browser.new(WB_SEARCH).run
+#         position = page.find_element(xpath: '//*[@id="c111885412"]').attribute("data-card-index")
+#
+#
+#         product.keywords.find_by(name: search)
+#       end
+#     end
+#   end
+# end
