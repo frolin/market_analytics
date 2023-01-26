201d758fd89360e2b21b28c61aa499e5fafdc91a
Andrey Frolov <a.frolov@sftpro.ru>
2023-01-27 00:44:35 +0300

    fix migration

--- db/migrate/20230109221905_add_store_to_stock.rb
+++ db/migrate/20230109221905_add_store_to_stock.rb
@@ -3,7 +3,7 @@ class AddStoreToStock < ActiveRecord::Migration[7.0]
 
     remove_reference :stocks, :product
     add_reference :stocks, :store, null: true, foreign_key: true
-    Stock.update_all(store_id: Store.last.id)
+    Stock.update_all(store_id: Store.last.id) if Store.last.present?
 
     change_column_null :stocks, :store_id, false
   end
--- db/migrate/20230119180010_add_account_to_store.rb
+++ db/migrate/20230119180010_add_account_to_store.rb
@@ -1,11 +1,14 @@
 class AddAccountToStore < ActiveRecord::Migration[7.0]
-  def change
-    add_reference :stores, :account, null: true, foreign_key: true
 
-    Store.find_each do |store|
-      store.update!(account: Account.first)
+  ActiveRecord::Schema.define do
+    def change
+      add_reference :stores, :account, null: true, foreign_key: true
+
+      Store.find_each do |store|
+        store.update!(account: Account.first) if store.account.blank? && Account.first.present?
+      end
+
+      change_column_null :stores, :account_id, false
     end
-
-    change_column_null :stores, :account_id, false
   end
 end
