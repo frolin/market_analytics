6adc5e9ebb9a658a2bcb5bbd5d0eba2be8ce6a14
Andrey Frolov <a.frolov@sftpro.ru>
2023-02-01 17:14:28 +0300

    fix stores hook

--- app/controllers/telegram_webhooks_controller.rb
+++ app/controllers/telegram_webhooks_controller.rb
@@ -45,7 +45,7 @@ class TelegramWebhooksController < Telegram::Bot::UpdatesController
 
   def stock!
     tg_user.user.stores.each do |store|
-      ::Telegram::Notifications::StocksNew.new(store).call
+      ::Telegram::Notifications::StocksNew.new(store, tg_user.user).call
     end
   end
 
